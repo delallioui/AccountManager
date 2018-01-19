30e99dc04694e78aa21c22a3dcb08f252e9e2414
driss.el-allioui <driss.el-allioui@POR-TSA-DELA.corp.talan.com>
2018-01-19 16:51:30 +0100

    add constants

---
+++ src/main/java/org/talan/account/manager/utils/AccountManagerConstants.java
@@ -0,0 +1,11 @@
+package org.talan.account.manager.utils;
+
+public class AccountManagerConstants {
+
+	public static final String AMOUNT_CAN_T_BE_NEGATIVE = "amount can't be negative";
+	public static final String PAYEE_CAN_T_BE_NULL = "payee can't be null";
+	public static final String PAYER_CAN_T_BE_NULL = "payer can't be null";
+	public static final String ACCOUNT_CAN_T_BE_NULL = "Account can't be null";
+	public static final String BALANCE_CANT_BE_LOWER_THAN_AMOUNT = "balance cant be lower than amount";
+
+}
